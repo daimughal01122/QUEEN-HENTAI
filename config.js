f02c34a5443e11ae33d6b5066a2c900c9b828ef9
daimughal01122 <131016664+daimughal01122@users.noreply.github.com>
2023-10-24 11:55:37 +0500

    config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94740804536', 'Dinuwa Official²⁰²³', true],
-  ['94740804536', 'Dinuwa Official²⁰²³', true], 
+  ['94740804536', 'Dani Official²⁰²³', true],
+  ['94740804536', 'Dani Official²⁰²³', true], 
   [''] 
 ] //Number of owners
 
-global.mods = ['94740804536','94740804536'] 
-global.prems = ['94740804536', '94740804536', '94740804536']
+global.mods = ['923140870854','923140870854'] 
+global.prems = ['923140870854', '923140870854', '923140870854']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -47,19 +47,18 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = '💝 Queen Hentai 💝'
+global.botname = '🐼 DANI KA BOT 🐼'
 global.premium = 'true'
-global.packname = '💝 Queen Hentai 💝 Created By 💝 Dinuwa Official²⁰²³ 💝' 
-global.author = '💝 Dinuwa Official²⁰²³ 💝' 
-global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/Dinuwa Official²⁰²³\n' 
-global.dygp = 'https://chat.whatsapp.com/Jyjqx7KBfgjDC7QcefHB1S'
-global.fgsc = 'https://github.com/dinuwah/QUEEN-HENTAI' 
-global.fgyt = 'https://chat.whatsapp.com/Jyjqx7KBfgjDC7QcefHB1S'
+global.packname = '🐼 PANDA 🐼 Created By 🐼 Dani Official²⁰²³ 🐼' 
+global.author = '🐼 Dani Official²⁰²³ 🐼' 
+global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/itx.dani._²⁰²³\n' 
+global.dygp = 'https://chat.whatsapp.com/KqVjiVsT3OIB90DgfA7k47' 
+global.fgyt = 'https://chat.whatsapp.com/KqVjiVsT3OIB90DgfA7k47'
 global.fgpyp = 'https://chat.whatsapp.com/Jyjqx7KBfgjDC7QcefHB1S'
 global.fglog = 'https://i.ibb.co/T1DqdYz/thumbnail.jpg' 
 
 
-global.wait = '*💝 _Waiting Bro Uploading by 💝 Queen Hentai 💝..._*'
+global.wait = '*💝 _Waiting Bro Uploading by 🐼 Queen Hentai 🐼..._*'
 global.rwait = '🇱🇰'
 global.dmoji = '🤣'
 global.done = '👍'
